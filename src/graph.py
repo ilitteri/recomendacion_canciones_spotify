d08a1fcf40d709aef76707fa3cbe179bd5cb7445
ilitteri <ilitteri@fi.uba.ar>
2021-02-25 22:30:41 -0300

    removed probably unuseful stuff

--- src/graph.py
+++ src/graph.py
@@ -1,93 +1,85 @@
+from typing import Iterator
+
 class Vertex:
     def __init__(self, label: str):
-        self.label = label
-        # self.eccentricity = None #se asume el vertice aislado
-        self.adjacents = set()
-        self.incidentEdges = set()
+        self.__label: str = label
+        # self.__eccentricity = None #se asume el vertice aislado
+        self.__adjacents: set = set()
+        self.__incidentEdges: set = set()
     
     def __str__(self):
-        return f'{self.label}'
+        return f'{self.__label}'
     
     def __iter__(self):
-        return iter(self.adjacents)
+        return iter(self.__adjacents)
 
     def getLabel(self) -> str:
-        return self.label
+        return self.__label
     
     def getDegree(self) -> int:
-        return len(self.incidentEdges)
+        return len(self.__incidentEdges)
     
     # def getEccentricity(self) -> float:
-    #     return self.eccentricity if self.eccentricity != None else float('inf')
+    #     return self.__eccentricity if self.__eccentricity != None else float('inf')
 
     def getAdjacents(self) -> set:
-        return self.adjacents
+        return self.__adjacents
     
     def isAdjacentOf(self, other) -> bool:
-        return other in self.adjacents
+        return other in self.__adjacents
     
     def getAdjacentsCount(self) -> int:
-        return len(self.adjacents)
+        return len(self.__adjacents)
     
     def isIsolated(self) -> bool:
-        return len(self.adjacents) == 0 and len(self.incidentEdges) == 0
+        return len(self.__adjacents) == 0 and len(self.__incidentEdges) == 0
 
     def addAdjacent(self, other) -> None:
-        self.adjacents.add(other)
+        self.__adjacents.add(other)
     
     def addIncidentEdge(self, edge) -> None:
-        self.incidentEdges.add(edge)
+        self.__incidentEdges.add(edge)
 
 class Edge:
     def __init__(self, v1, v2):
-        self.endPoints = set([v1, v2])
+        self.__endPoints = set([v1, v2])
 
     def __iter__(self):
-        return iter(self.endPoints)
+        return iter(self.__endPoints)
     
     def __str__(self):
-        return f'{self.endPoints[0]}<--->{self.endPoints[1]}'
+        return f'{self.__endPoints[0]}<--->{self.__endPoints[1]}'
 
     def getEndPoints(self) -> set:
-        return self.endPoints
+        return self.__endPoints
     
     def isLoop(self) -> bool:
-        return self.endPoints[0] == self.endPoints[1]
+        return self.__endPoints[0] == self.__endPoints[1]
         
 class Graph:
-    def __init__(self, verticesSet: dict = None, edgesSet: set = None, isDigraph: bool = False):
-        self.verticesSet = verticesSet or {}
-        self.edgesSet = edgesSet or set()
-        self.isDigraph = isDigraph
-
-        self.radius = None
-        self.diameter = None
-        self.center = set()
-        self.periphery = set()
-        self.verticesEccentricities = {}
-        self.verticesDegrees = {}
-
-        self._dirty = False
+    def __init__(self, verticesSet: dict = None, edgesSet: set = None):
+        self.__verticesSet: dict = verticesSet or {}
+        self.__edgesSet: set = edgesSet or set()
     
-    def __iter__(self):
-        return iter(self.verticesSet)
+    def __iter__(self) -> Iterator:
+        return iter(self.__verticesSet)
     
-    def __contains__(self, vertex):
-        return vertex in self.verticesSet
+    def __contains__(self, vertex) -> bool:
+        return vertex in self.__verticesSet
     
-    def __len__(self):
-        return len(self.verticesSet)
+    def __len__(self) -> int:
+        return len(self.__verticesSet)
     
     def __add__(self, other):
-        return Graph({**self.verticesSet, **other.verticesSet}, self.edgesSet+other.edgesSet)
-        #Py3.9#return Graph(self.verticesSet | other.verticesSet, self.edgesSet+other.edgesSet)
+        return Graph({**self.__verticesSet, **other.verticesSet}, self.__edgesSet+other.edgesSet)
+        #Py3.9#return Graph(self.__verticesSet | other.verticesSet, self.__edgesSet+other.edgesSet)
 
     def isConnected(self) -> bool:
-        if len(self.verticesSet) == 0:
+        if len(self.__verticesSet) == 0:
             return True
         visited = set()
         q = []
-        origin = self.verticesSet.values()[0]
+        origin = self.__verticesSet.values()[0]
         visited.add(origin)
         q.append(origin)
         while len(q) != 0:
@@ -96,110 +88,59 @@ class Graph:
                 if not adjacent in visited:
                     visited.add(adjacent)
                     q.append(adjacent)
-        return self.verticesSet == visited
-
-    def update(self) -> None:
-        self.verticesDegrees.update({vertex.getLabel():vertex.getDegree() for vertex in self})
-        if self.isConnected():
-            self.verticesEccentricities.update({vertex.getLabel():self.getEccentricityOf(vertex) for vertex in self})
-            verticesEccentricities = self.verticesEccentricities.values()
-            self.radius = min(verticesEccentricities)
-            self.diameter = max(verticesEccentricities)
-            self.center = list(filter(min, verticesEccentricities))
-            self.periphery = list(filter(max, verticesEccentricities))
-        else:
-            self.radius = float('inf')
-            self.diameter = float('inf')
-            self.verticesEccentricities = {vertex.getLabel():float('inf') for vertex in self}
-            self.center = list()
-            self.periphery = list()
+        return self.__verticesSet == visited
 
     def addVertex(self, vertex) -> None:
-        if vertex.getLabel() not in self.verticesSet:
-            self.verticesSet[vertex.getLabel()] = vertex
-            self.verticesDegrees[vertex.getLabel()] = vertex.getDegree()
+        if vertex.getLabel() not in self.__verticesSet:
+            self.__verticesSet[vertex.getLabel()] = vertex
+            self.__verticesDegrees[vertex.getLabel()] = vertex.getDegree()
         else:
             raise ValueError("Duplicate vertex")
-        self._dirty = True
     
     def addEdge(self, edge) -> None:
         endPoints = list(edge.getEndPoints())
         for vertex in endPoints:
-            if vertex.getLabel() not in self.verticesSet:
+            if vertex.getLabel() not in self.__verticesSet:
                 raise ValueError("Vertex not in graph")
         endPoints[0].addAdjacent(endPoints[1])
         endPoints[1].addIncidentEdge(edge)
-        if not self.isDigraph:
+        if not self.__isDigraph:
             endPoints[1].addAdjacent(endPoints[0])
             endPoints[0].addIncidentEdge(edge)
-        self.edgesSet.add(edge)
-        self._dirty = True
+        self.__edgesSet.add(edge)
     
+    def removeVertex(self) -> Vertex:
+        pass
+
+    def removeEdge(self) -> Edge:
+        pass
+
     def getVertices(self) -> dict:
-        return self.verticesSet
+        return self.__verticesSet
     
-    def getVertex(self, label: str):
+    def getVertex(self, label: str) -> Vertex:
         if label in self:
-            return self.verticesSet[label]
+            return self.__verticesSet[label]
         raise NameError(label)
+
+    def getEdge(self) -> Edge:
+        pass
     
     def getEdges(self) -> set:
-        return self.edgesSet
+        return self.__edgesSet
 
     def getOrder(self) -> int:
         return len(self)
     
     def getSize(self) -> int:
-        return len(self.edgesSet)
+        return len(self.__edgesSet)
 
     def getDegree(self) -> int:
         # by theorem
-        return 2 * len(self.edgesSet)
+        return 2 * len(self.__edgesSet)
     
     def getMinDegree(self) -> int:
-        return min(self.verticesDegrees.values())
+        return min(self.__verticesDegrees.values())
     
     def getMaxDegree(self) -> int:
-        return max(self.verticesDegrees.values())
-    
-    def isNull(self) -> bool:
-        return len(self.verticesSet) == 0 and len(self.edgesSet) == 0
-
-    # def isSubgraphOf(self, other) -> bool:
-    #     isSubgraph = True
-    #     for v in self:
-    #         isSubgraph &= v in other
-    #     isSubgraph &= self.edgesSet <= other.edgesSet
-    #     return isSubgraph
-    
-    def getEccentricityOf(self, vertex) -> int:
-        # max d(u,v)
-        pass
-
-    def getRadius(self) -> int:
-        # min eccentricity
-        if self._dirty:
-            self.update()
-            self._dirty = False
-        return self.radius
-
-    def getDiameter(self) -> int:
-        # max eccentricity
-        if self._dirty:
-            self.update()
-            self._dirty = False
-        return self.diameter
-    
-    def getPeriphery(self):
-        # set o peripheral points
-        if self._dirty:
-            self.update()
-            self._dirty = False
-        return self.periphery
-    
-    def getCenter(self):
-        # set of central points
-        if self._dirty:
-            self.update()
-            self._dirty = False
-        return self.center
+        return max(self.__verticesDegrees.values())
