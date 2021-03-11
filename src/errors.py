235c40c6e6def94610ceea879eca86e1b852c5e6
ilitteri <ilitteri@fi.uba.ar>
2021-03-11 08:54:27 -0300

    added constant files for error msgs & simple constants

---
+++ src/errors.py
@@ -0,0 +1,3 @@
+ERROR_INPUT = 'ERROR: Entrada Invalida'
+ERROR_CMD = 'ERROR: Comando Ingresado Invalido (no existe o esta mal escrito)'
+ERROR_PARAM_COUNT = 'ERROR: Cantidad de Parametros Invalida para este Comando'
